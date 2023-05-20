ce59175b8915ce9cc2433eacfc1a8ce6c9285228
Brandon P.C. Yeh <57129342+brandon9912@users.noreply.github.com>
2023-05-20 15:53:52 +0800

    feat: 🎸 create draggable and droppable cards

--- frontend/src/components/placeItem.jsx
+++ frontend/src/components/placeItem.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from "@chakra-ui/react";
 
 const PlaceItem = ({ place }) => {
   return (
-    <Box>
-      <Heading as="h2" size="md" mb={2}>
-        {place.name}
-      </Heading>
-    </Box>
+    // <Box id={place.id}>
+    //   <Heading as="h2" size="md" mb={2}>
+    //     {place.name}
+    //   </Heading>
+    //   <Text mb={2}>Rating Amount: {place.user_ratings_total}</Text>
+    //   <Text mb={2}>Rating: {place.rating}</Text>
+    // </Box>
+    <Card direction={{ base: column, sm: "row" }}>
+      <Image objectFit="cover" maxW={{ base: "100%", sm: "200px" }} />
+
+      <CardHeader>{place.name}</CardHeader>
+      <CardBody>
+        <Text mb={2}>Rating Amount: {place.user_ratings_total}</Text>
+        <Text mb={2}>Rating: {place.rating}</Text>
+      </CardBody>
+      <CardFooter>Footer</CardFooter>
+    </Card>
   );
 };
 
