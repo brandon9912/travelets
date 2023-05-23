d259f8a56b1930ac10c87aa884b84346105aa3f9
Brandon P.C. Yeh <57129342+brandon9912@users.noreply.github.com>
2023-05-23 22:03:37 +0800

    feat: 🎸 add trip create api

--- frontend/src/components/trip/tripCreate.jsx
+++ frontend/src/components/trip/tripCreate.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import axios from "axios";
 import {
   Flex,
+  Box,
+  Button,
   Stack,
   FormControl,
   FormLabel,
@@ -9,6 +12,28 @@ import {
 } from "@chakra-ui/react";
 
 const tripCreate = () => {
+  const [tripName, setTripName] = useState("");
+  const [destination, setDestination] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(tripName, destination, startDate, endDate);
+    axios
+      .post("http://localhost:3005/trip/create", {
+        tripName: tripName,
+        destination: destination,
+        startDate: startDate,
+        endDate: endDate,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <Flex
@@ -19,16 +44,27 @@ const tripCreate = () => {
       >
         <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
           <Stack align={"center"}>
-            <FormControl>
-              <FormLabel>Trip Name</FormLabel>
-              <Input type="text" />
-              <FormLabel>Destination</FormLabel>
-              <Input type="text" />
-              <FormLabel>Start Date</FormLabel>
-              <Input type="date" />
-              <FormLabel>End Date</FormLabel>
-              <Input type="date" />
-            </FormControl>
+            <Box>
+              <FormControl>
+                <FormLabel>Trip Name</FormLabel>
+                <Input type="text" />
+                <FormLabel>Destination</FormLabel>
+                <Input type="text" />
+                <FormLabel>Start Date</FormLabel>
+                <Input type="date" />
+                <FormLabel>End Date</FormLabel>
+                <Input type="date" />
+              </FormControl>
+              <Button
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Create Trip
+              </Button>
+            </Box>
           </Stack>
         </Stack>
       </Flex>
